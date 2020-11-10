e71ff3c43667da11d552bba228fc0879533cc90d
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-09 23:40:51 -0500

    added private routes

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -20,6 +20,10 @@ export function AuthProvider({ children }) {
         return auth.signInWithEmailAndPassword(email, password)
     }
 
+    function logout() {
+        return auth.signOut()
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
@@ -33,7 +37,8 @@ export function AuthProvider({ children }) {
     const value = {
         currentUser,
         login,
-        signup
+        signup,
+        logout
     }
 
 
