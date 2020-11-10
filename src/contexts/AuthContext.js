1443d2086d9abb29eba6d10c9979366dbd305823
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-10 00:27:14 -0500

    Add update profile

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -24,6 +24,18 @@ export function AuthProvider({ children }) {
         return auth.signOut()
     }
 
+    function resetPassword(email){
+      return auth.sendPasswordResetEmail(email)
+    }
+
+    function updateEmail(email) {
+      return currentUser.updateEmail(email)
+    }
+
+    function updatePassword(password) {
+      return currentUser.password(password)
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
@@ -38,7 +50,10 @@ export function AuthProvider({ children }) {
         currentUser,
         login,
         signup,
-        logout
+        logout,
+        resetPassword,
+        updateEmail,
+        updatePassword
     }
 
 
