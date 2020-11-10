63fad1afd2fed31e06114bcb9605e0d39de75514
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-09 23:15:50 -0500

    Add dashboard component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext"
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 export default function Signup() {
     const emailRef = useRef()
@@ -10,7 +10,7 @@ export default function Signup() {
     const { signup } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
-    const 
+    const history = useHistory()
 
     async function handleSubmit(e) {
         e.preventDefault()
